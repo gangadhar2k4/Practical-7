b92e613de6a3a9bd52ed3d3ea2f8cc12dbe64c5d
gangadhar2k4 <157013438+gangadhar2k4@users.noreply.github.com>
2024-01-21 21:30:24 +0530

    Update PalindromeString.py

--- PalindromeString.py
+++ PalindromeString.py
@@ -5,8 +5,6 @@ file.write(str)
 file.write('\n')
 Temp=str[::-1]
 if str==Temp:
-    print("True")
     file.write("Given string is palindrome")
 else:
-    print("False")
     file.write("Given string is not a palindrome")
